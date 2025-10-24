7407a8e939b48063afd8396ca52e01f871d0122c
Dragana Savanovic <dsavanovic@inimco.com>
2025-10-24 14:57:45 +0200

    added async pipe

--- src/app/messages/messages-list/messages-list.component.ts
+++ src/app/messages/messages-list/messages-list.component.ts
@@ -1,32 +1,21 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MessagesService } from '../messages.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-messages-list',
   standalone: true,
+  imports: [AsyncPipe],
   templateUrl: './messages-list.component.html',
   styleUrl: './messages-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MessagesListComponent implements OnInit {
+export class MessagesListComponent {
 
   private messageService = inject(MessagesService);
-  private cdRef = inject(ChangeDetectorRef);
-  messages: string[] = [];
-  private destroyRef = inject(DestroyRef);
+  messages$ = this.messageService.messages$;
 
   // messages = this.messageService.allMessages;
-
-  ngOnInit(): void {
-    const subscription = this.messageService.messages$.subscribe((messages) => {
-      this.messages = messages;
-      this.cdRef.markForCheck();
-    });
-
-    this.destroyRef.onDestroy(() => {
-      subscription.unsubscribe();
-    });
-  }
 
   // get messages() {
   //   return this.messageService.allMessages;
